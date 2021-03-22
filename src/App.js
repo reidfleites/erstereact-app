65e69c03d8a07908c6320ab25810fc32c3cab99b
reidfleites <dreinier86@gmail.com>
2021-03-22 14:51:18 +0100

    local react app

--- src/App.js
+++ src/App.js
@@ -1,24 +1,53 @@
-import logo from './logo.svg';
-import './App.css';
+
+import './App.scss';
+import imag1 from './images/clement-helardot-95YRwf6CNw8-unsplash.jpg';
+import imag2 from './images/freddie-marriage-vSchPA-YA_A-unsplash.jpg';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div>
+      <div>
+          <ul>
+              <li>
+                  <a href="index.html">Home</a> 
+              </li>
+              <li>
+                  <a href="">DCI-Projekte</a> 
+              </li>
+              <li>
+                  <a href="">Personal-Projekte</a> 
+              </li>
+          </ul>
+       </div>
+       <div>
+         <img src={imag1} alt=""/>
+       
+       </div>
+  </div> 
+  <div id="section-2">
+        <div id="element-1">
+        
+            <div id="sub-element-1-1">
+                <h2>Learning Plan</h2>
+            </div>
+            <div>
+                <ol>
+                    <li><a href="http://">HTML5</a></li>
+                    <li><a href="http://">CSS3</a></li>
+                    <li><a href="http://">JAVA SCRIPT ES6</a></li>
+                    <li><a href="http://">REACT</a></li>
+                </ol>
+            </div>
+        </div>
+        
+
+        <div id="element-2">
+            <img  src={imag2} alt=""/>
+        </div>
+  </div>
     </div>
+
   );
 }
 
